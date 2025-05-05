378139a14a1e4099473f10c9e017d4427a14f81e
Thanh Phi <thanhphichv96@gmail.com>
2025-05-05 16:51:42 +0700

    lấy dữ liệu ví tổng & lưu vào file

--- walletmanager/data_manager.cpp
+++ walletmanager/data_manager.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <filesystem>
 #include <iostream>
+#include <sstream>
+
 namespace fs = std::filesystem;
 
 using namespace std;
@@ -36,10 +38,50 @@ bool DataManager::loadTransactions(std::vector<User>& users, const std::string& 
 
 // Lưu data của ví tổng xuống file
 bool DataManager::saveGlobalWallet(const Wallet& wallet, const string& filename) {
+    ofstream out(filename);
+    if (!out.is_open()) return false;
+
+    out << wallet.getWalletId() << "|" << wallet.getBalance() << "\n";
+    for (const auto& tx : wallet.getTransactionHistory()) {
+        out << tx.getTimestamp() << "|" << tx.getFromWalletId() << "|"
+            << tx.getToWalletId() << "|" << tx.getAmount() << "|"
+            << tx.getStatus() << "\n";
+    }
+
+    out.close();
     return true;
 }
 
 // Load dữ liệu ví tổng
 bool DataManager::loadGlobalWallet(Wallet& wallet, const string& filename) {
+    ifstream in(filename);
+    if (!in.is_open()) return false;
+
+    string line;
+    getline(in, line);
+    istringstream ss(line);
+    string walletId;
+    int balance;
+    getline(ss, walletId, '|');
+    ss >> balance;
+
+    wallet = Wallet(walletId);
+    wallet.adjustBalance(balance);
+
+    while (getline(in, line)) {
+        istringstream txs(line);
+        string ts, from, to, status;
+        int amt;
+        getline(txs, ts, '|');
+        getline(txs, from, '|');
+        getline(txs, to, '|');
+        txs >> amt;
+        txs.ignore(); getline(txs, status);
+
+        Transaction tx(from, to, amt, ts, status);
+        wallet.addTransaction(tx);
+    }
+
+    in.close();
     return true;
 }
